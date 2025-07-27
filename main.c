05955c1e6bb51d40a9aeafb7421cdfb9ddeb92d0
ni <nicolas.nkruger@gmail.com>
2025-07-27 15:29:54 -0300

    🐝

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <X11/X.h>
 #include <X11/Xlib.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,6 +6,11 @@
 struct screen_size {
   int width;
   int height;
+};
+
+struct cmd_opt {
+  char *screen_name;
+  struct screen_size screen_size;
 };
 
 struct screen_size get_screen_size() {
@@ -20,14 +26,14 @@ struct screen_size get_screen_size() {
   return size;
 }
 
-int main(int argc, char *argv[]) {
+char *cmd_gen(struct cmd_opt _cmd) {
 
-  const char *display = getenv("DISPLAY");
+  const int buffer_size = 512;
 
   const char *cmd_temp =
       "ffmpeg -video_size %s -framerate 25 -f x11grab -i %s .mkv";
 
-  char cmd[512];
+  char *cmd = malloc(buffer_size);
 
   struct screen_size screen_size = get_screen_size();
 
@@ -36,7 +42,18 @@ int main(int argc, char *argv[]) {
   snprintf(display_size, sizeof(display_size), display_size_temp,
            screen_size.width, screen_size.height);
 
-  snprintf(cmd, sizeof(cmd), cmd_temp, display_size, display);
+  snprintf(cmd, buffer_size, cmd_temp, display_size, _cmd.screen_name);
+
+  return cmd;
+}
+
+int main(int argc, char *argv[]) {
+
+  struct cmd_opt _cmd_opt;
+  _cmd_opt.screen_name = getenv("DISPLAY");
+  _cmd_opt.screen_size = get_screen_size();
+
+  char *cmd = cmd_gen(_cmd_opt);
 
   printf("%s", cmd);
 
@@ -44,5 +61,7 @@ int main(int argc, char *argv[]) {
   //     "ffmpeg -video_size 1366x768 -framerate 25 -f x11grab -i :1 .mkv";
   // system(cmd);
   // printf("record");
+
+  free(cmd);
   return 0;
 }
